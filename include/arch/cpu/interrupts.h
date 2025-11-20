f4f4854c74238681a64eb41bd372638129e891aa
melonSorbet <kilicyunusemre678@gmail.com>
2025-11-20 18:38:34 +0100

    chore: fix printing and format

--- include/arch/cpu/interrupts.h
+++ include/arch/cpu/interrupts.h
@@ -3,28 +3,23 @@
 
 #include <stdint.h>
 
-
-
 typedef struct {
-    uint32_t r0;
-    uint32_t r1;
-    uint32_t r2;
-    uint32_t r3;
-    uint32_t r4;
-    uint32_t r5;
-    uint32_t r6;
-    uint32_t r7;
-    uint32_t r8;
-    uint32_t r9;
-    uint32_t r10;
-    uint32_t r11;
-    uint32_t r12;
-    uint32_t lr;
-    uint32_t spsr;   // <-- save SPSR here
+	uint32_t r0;
+	uint32_t r1;
+	uint32_t r2;
+	uint32_t r3;
+	uint32_t r4;
+	uint32_t r5;
+	uint32_t r6;
+	uint32_t r7;
+	uint32_t r8;
+	uint32_t r9;
+	uint32_t r10;
+	uint32_t r11;
+	uint32_t r12;
+	uint32_t lr;
+	uint32_t spsr; // <-- save SPSR here
 } exc_frame_t;
-
-
-
 
 /* Exception C handlers */
 void software_interrupt_c(exc_frame_t *frame);
