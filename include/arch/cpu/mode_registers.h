0c4797e0e0c0a6d183a39a5b2a254b6e6cfeb828
melonSorbet <kilicyunusemre678@gmail.com>
2025-11-23 18:48:06 +0100

    maybe

--- include/arch/cpu/mode_registers.h
+++ include/arch/cpu/mode_registers.h
@@ -19,24 +19,63 @@ struct mode_regs {
 };
 
 
+
 static inline struct mode_regs read_mode_specific_registers(void) {
     struct mode_regs regs = {0};
-
-
     unsigned int sp, lr;
+
+    // Supervisor mode
     asm volatile(
+        "cps #0x13\n\t"     // Switch to SVC
         "mov %0, sp\n\t"
         "mov %1, lr\n\t"
         : "=r"(sp), "=r"(lr)
     );
-
-
     regs.supervisor_sp = sp;
     regs.supervisor_lr = lr;
 
+    // IRQ mode
+    asm volatile(
+        "cps #0x12\n\t"
+        "mov %0, sp\n\t"
+        "mov %1, lr\n\t"
+        : "=r"(sp), "=r"(lr)
+    );
+    regs.irq_sp = sp;
+    regs.irq_lr = lr;
+
+    // Abort mode
+    asm volatile(
+        "cps #0x17\n\t"
+        "mov %0, sp\n\t"
+        "mov %1, lr\n\t"
+        : "=r"(sp), "=r"(lr)
+    );
+    regs.abort_sp = sp;
+    regs.abort_lr = lr;
+
+    // Undefined mode
+    asm volatile(
+        "cps #0x1B\n\t"
+        "mov %0, sp\n\t"
+        "mov %1, lr\n\t"
+        : "=r"(sp), "=r"(lr)
+    );
+    regs.undefined_sp = sp;
+    regs.undefined_lr = lr;
+
+    // User/System mode
+    asm volatile(
+        "cps #0x1F\n\t"
+        "mov %0, sp\n\t"
+        "mov %1, lr\n\t"
+        : "=r"(sp), "=r"(lr)
+    );
+    regs.user_sp = sp;
+    regs.user_lr = lr;
+
     return regs;
 }
-
 #include <stdint.h>
 
 /* Data Fault Status Register (DFSR) */
